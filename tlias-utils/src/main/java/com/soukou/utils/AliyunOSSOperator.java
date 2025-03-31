48ba6dae8a22d4a8c0c37d7c34054d3fd1912967
z1y6h <947934464@qq.com>
2025-03-31 14:46:30 +0800

    Maven分模块设计与开发（提取pojo和utils类）

--- tlias-utils/src/main/java/com/soukou/utils/AliyunOSSOperator.java
+++ tlias-utils/src/main/java/com/soukou/utils/AliyunOSSOperator.java
@@ -1,11 +1,14 @@
 package com.soukou.utils;
 
-import com.aliyun.oss.*;
+import com.aliyun.oss.ClientBuilderConfiguration;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.common.auth.CredentialsProviderFactory;
 import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
 import com.aliyun.oss.common.comm.SignVersion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 import java.io.ByteArrayInputStream;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
